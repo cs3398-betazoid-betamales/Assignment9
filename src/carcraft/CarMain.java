168bbba16dc312c3352f7f1ac4aac1852d48bf05
CWhitfield91 <cameronrw91@gmail.com>
2019-03-13 20:37:59 -0500

    Update CarMain.java
    Added one more car

--- src/carcraft/CarMain.java
+++ src/carcraft/CarMain.java
@@ -8,9 +8,10 @@ public class CarMain {
 
 		System.out.println("Starting:  Creating Cars without Decorator Pattern\n");
 		ICar basicCar = new BasicCar();
+		System.out.println(basicCar.GetDescription());
 
 		System.out.println("Make a Supercharged Basic Car:");
-    ICar SuperCharger = new SuperCharger(basicCar);
+        ICar SuperCharger = new SuperCharger(basicCar);
 		System.out.println(SuperCharger.GetDescription());
 
 		System.out.println("Make a Basic Car with an Air Scoop:");
@@ -24,6 +25,10 @@ public class CarMain {
 		System.out.println("Super charged blue car with air scoop.");
 		ICar prettyMuscleCar = new PaintColor(new SuperCharger(new AirScoop(new BasicCar())), "Blue");
 		System.out.println(prettyMuscleCar.GetDescription());
+
+		System.out.println("Red car with sports wheels:");
+		ICar redCarWithSportWheels = new SportWheels(new PaintColor(new BasicCar(), "Red"));
+		System.out.println(redCarWithSportWheels.GetDescription());
 
 
 	}
